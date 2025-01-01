f375b77579f7fe489f518d42da28fea0761c3ecc
RamadeviPaladugu <ramadevi.paladugu@gmail.com>
2025-01-01 17:31:24 -0500

    added why cook with us

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,30 +1,24 @@
 import React from 'react';
-
-
-
-import  JoinCommunity  from '../components/JoinCommunity';
-
+import FeaturedCardProps from '../components/FeaturedCardProps';  
+import JoinCommunity from '../components/JoinCommunity';
 import { PopularRecipes } from '../components/Popular Recipes/PopularRecipes';
 import FooterSection from '../components/Footer/FooterSection';
 import { CategoryGrid } from '../components/CategoryGrid';
 
-
-
-
-
 export default function Home() {
   return (
     <>
+      <FeaturedCardProps />
+
       <PopularRecipes />
 
       <CategoryGrid />
 
       <JoinCommunity />
-      
-      <FooterSection />
 
-      
+      <FooterSection />
     </>
   );
 }
+
  
