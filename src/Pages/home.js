d0eda15a0a80890c8e15bcfac8811557cb37174f
TSA-WR-WEB-DESIGN <tsa.wr.web.design@gmail.com>
2025-01-09 20:29:22 -0500

    Add RecipeQuizCard component to home page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -5,12 +5,14 @@ import { PopularRecipes } from '../components/Popular Recipes/PopularRecipes';
 import FooterSection from '../components/Footer/FooterSection';
 import { CategoryGrid } from '../components/CategoryGrid';
 import A from '../components/A'; // Updated import statement
+import RecipeQuizCard from '../components/RecipeQuizCard';
 
 export default function Home() {
   return (
     <>
       <A />
-
+      
+      <RecipeQuizCard />
       <FeaturedCardProps />
 
       <PopularRecipes />
